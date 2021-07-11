0813624698f336b532c61bf31d1885bbf8fe5a35
AlirezaPNouri <apashamoham@miners.utep.edu>
2021-07-10 19:50:24 -0600

    add new classes

---
+++ codes/pre-processing.py
@@ -0,0 +1,6 @@
+"""
+Author: Alireza P. Nouri
+Advisor: prof. M. Shahriar hossain
+contact: apashamoham@miners.utep.edu
+Implementing feature exrtaction by BERT model
+"""
---
+++ codes/temp.py
@@ -0,0 +1,12 @@
+"""
+Author: Alireza P. Nouri
+Advisor: prof. M. Shahriar hossain
+contact: apashamoham@miners.utep.edu
+Implementing feature exrtaction by BERT model
+"""
+# import packages
+import numpy as np
+import pandas as pd
+# read database
+df = pd.read_csv('../../../datasets/Bitcoin_tweets/Bitcoin_tweets.csv', 'r')
+print('it works')
